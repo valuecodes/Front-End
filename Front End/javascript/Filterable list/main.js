6c8e3c1f64fe7f1f8d66bc5d8953f16e73a2c144
Juha Kangas <juhakangas5@gmail.com>
2018-08-07 15:19:19 +0300

    added headerfilter to filterable list

--- Front End/javascript/Filterable list/main.js
+++ Front End/javascript/Filterable list/main.js
@@ -1,25 +1,51 @@
-//Get input element
+// Get input element
 let filterInput = document.getElementById('filterInput');
-
+// Add event listener
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames(){
   // Get value of input
-  let filterValue=document.getElementById('filterInput').value.toUpperCase();
-  
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
   // Get names ul
-  let ul=document.getElementById('names');
+  let ul = document.getElementById('names');
   // Get lis from ul
   let li = ul.querySelectorAll('li.collection-item');
-  
-  // Loop trough collection-item lis
-  for(let i = 0;li.length;i++){
-    let a=li[i].getElementsByTagName('a')[0];
-    //IF matched
-    if(a.innerHTML.toUpperCase().indexOf(filterValue)>-1){
-      li[i].style.display='';
-    }else{
-      li[i].style.display='none';
+  let head = ul.querySelectorAll('li.collection-header');
+
+  // Loop through collection-item lis
+  for(let i = 0;i < li.length;i++){
+    let a = li[i].getElementsByTagName('a')[0];
+    // If matched
+    if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
     }
   }
+
+  // Loop through collection-headers lis
+  for(let i = 0;i < head.length;i++){
+    let b = head[i].getElementsByTagName('h5')[0];
+    
+    if(document.getElementById('filterInput').value == "") //Checking if form is empty
+    {
+      head[i].style.display = '';
+    }
+    // If matched
+    else if(b.innerHTML.toUpperCase().indexOf(filterValue[0]) > -1){
+      head[i].style.display = '';
+      
+
+
+    }else {
+      head[i].style.display = 'none';
+    }
+  }
+
 }
+
+
+
+
+
